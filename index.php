ffef417d159c5f25f006480e1a63a8d7f7ddb364
Hawk46340715 <23jn0344@jec.ac.jp>
2025-02-04 12:19:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_FILES['receipts']['tmp_nam
     }
 
     // OCR.pyを実行
+$output = shell_exec('python3 --version 2>&1');
+echo "<pre>$output</pre>";
+
+
     $output = shell_exec('python OCR.py ' . implode(' ', $file_paths) . ' 2>&1');
     file_put_contents('php_error.log', $output);
 
